0b71df54746fb425730db404bbda438cc502897a
w4t4 <winnie.4std@gmail.com>
2019-09-27 14:20:14 +0900

    delete luminance axis

--- src/colorCheck.m
+++ src/colorCheck.m
@@ -1,13 +1,12 @@
 load('ccmatrix.mat');
-load('../img/mag1_0.4/SDsameDragon.mat');
-a = SDsame(:,:,:,1);
-[ix,iy,iz] = size(SDsame(:,:,:,3));
+load('../img/mag3_dl/SDsameDragon.mat');
+[ix,iy,iz] = size(SDsame(:,:,:,1));
 po = zeros(ix,iy,iz);
 for i = 1:ix
     for j = 1:iy
         for k = 1:iz
             for l = 1:3
-                po(i,j,k) = po(i,j,k) + ccmatrix.xyz2rgb(k,l) * SDsame(i,j,l,9) / 3;
+                po(i,j,k) = po(i,j,k) + ccmatrix.xyz2rgb(k,l) * SDsame(i,j,l,1) / 3;
             end
             if po(i,j,k) > 1
                 po(i,j,k) = 1;
--- src/colorize_mat.m
+++ src/colorize_mat.m
@@ -1,11 +1,11 @@
 
 SDsame = colorize(maskedData, 1);
 SDdifferent = colorize(maskedDiffuse, 1) + colorize(maskedSpecular, 2);
-aveBrightness = zeros(3,9);
+aveBrightness = zeros(1,9);
 figure;
-montage(xyz2rgb(SDsame)/16, 'size', [3 9]);
+montage(xyz2rgb(SDsame)/16, 'size', [1 9]);
 figure;
-montage(xyz2rgb(SDdifferent)/16, 'size', [3 9]);
+montage(xyz2rgb(SDdifferent)/16, 'size', [1 9]);
 save('SDsameDragon.mat','SDsame');
 save('SDdifferentDragon.mat','SDdifferent');
 
@@ -18,24 +18,20 @@ function xyzData = colorize(xyzMaterial, flag)
     r2 = sqrt(2);
     uUnitCircle = [0 1 1/r2 0 -1/r2 -1 -1/r2 0 1/r2];
     vUnitCircle = [0 0 1/r2 1 1/r2 0 -1/r2 -1 -1/r2];
-    luminance = [1 0.4 0.16];
     colorDistanceRate = 35;
-    for i = 1:3
-        for j = 1:9
-            r = uvlMaterial;
-            if flag == 1
-                r(:,:,1) = r(:,:,1) + uUnitCircle(j)/colorDistanceRate;
-                r(:,:,2) = r(:,:,2) + vUnitCircle(j)/colorDistanceRate;
-            end
-            r(:,:,3) = r(:,:,3) .* luminance(i);
-            xyz = applycform(r,cu2x);
-            for k = 1:3
-                if xyz(120,120,k) < 0
-                    disp("oh....");
-                end
-            end
-            %meanLuminance(:,:,9*(i-1)+j) = calcMeanLuminance(xyz);
-            xyzData(:,:,:,9*(i-1)+j) = xyz;
+    for j = 1:9
+        r = uvlMaterial;
+        if flag == 1
+            r(:,:,1) = r(:,:,1) + uUnitCircle(j)/colorDistanceRate;
+            r(:,:,2) = r(:,:,2) + vUnitCircle(j)/colorDistanceRate;
         end
+        xyz = applycform(r,cu2x);
+        for k = 1:3
+            if xyz(120,120,k) < 0
+                disp("oh....");
+            end
+        end
+        %meanLuminance(:,:,9*(i-1)+j) = calcMeanLuminance(xyz);
+        xyzData(:,:,:,j) = xyz;
     end
 end
